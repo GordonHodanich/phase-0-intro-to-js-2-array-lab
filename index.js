39ff30033268414add4e5a6345357e74de2bf2b8
Gordon Hodanich <ghodanich@gmail.com>
2021-05-24 13:57:13 -0400

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(newCat){
+    cats.push(newCat)
+}
+
+function destructivelyPrependCat(newCat){
+    cats.unshift(newCat)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(newCat){
+    const copyOfCats = [...cats, `${newCat}`]
+    return copyOfCats
+}
+function prependCat(newCat){
+    const copyOfCats = [`${newCat}`, ...cats,]
+    return copyOfCats
+}
+function removeLastCat(){
+    const copyOfCats = [...cats.slice(0, -1)]
+    return copyOfCats
+}
+function removeFirstCat(){
+    const copyOfCats = [...cats.slice(1)]
+    return copyOfCats
+}
